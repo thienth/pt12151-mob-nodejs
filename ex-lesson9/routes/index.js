d29cb78dec5773917c47f61e23c39385d6695390
thienth32 <thienth32@gmail.com>
2017-10-12 13:02:29 +0700

    update

--- ex-lesson9/routes/index.js
+++ ex-lesson9/routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	// Doc du lieu tu post.json va user.json
+	// sau do hien thi o trang chu
+	
   res.render('index', { title: 'Express' });
 });
 

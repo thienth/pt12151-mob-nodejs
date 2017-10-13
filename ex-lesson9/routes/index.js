1919edb47ade300c935ab12ce054eb8faf8a5ffd
thienth32 <thienth32@gmail.com>
2017-10-13 07:14:46 +0700

    update

--- ex-lesson9/routes/index.js
+++ ex-lesson9/routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	// Doc du lieu tu post.json va user.json
 	// sau do hien thi o trang chu
-	
-  res.render('index', { title: 'Express' });
+	var user = fs.readFileSync(`${__dirname}/../data/user.json`, 'utf-8');
+	user = JSON.parse(user);
+	var post = fs.readFileSync(`${__dirname}/../data/post.json`, 'utf-8');
+	post = JSON.parse(post);
+
+  res.render('index', { title: 'Express', user: user, post: post });
 });
 
 module.exports = router;
